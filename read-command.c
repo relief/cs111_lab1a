af7d00fecab21444bd854d235c95b73e6b9dcb8b
relief <zjuxie@gmail.com>
2015-01-10 16:33:14 -0800

    first commit

--- read-command.c
+++ read-command.c
@@ -21,41 +21,65 @@
 #include <error.h>
 
 /* FIXME: You may need to add #include directives, macro definitions,
- static function definitions, etc.  */
+   static function definitions, etc.  */
 
 /* FIXME: Define the type 'struct command_stream' here.  This should
- complete the incomplete type declaration in command.h.  */
-
-/* A command stream is a linked list of command nodes, each containing a single command */
+   complete the incomplete type declaration in command.h.  */
 struct command_stream
 {
-    command_t command;
-    command_stream *next;
+      
+};
+command_t parseCmd(char * cmd){
+  command_t c = new command();
+  c->type = determineType(cmd);
+  switch (c->type){
+    pipe: c->command[0] = parseCmd(cmd[leftpart]);
+          c->command[1] = parseCmd(cmd[rightpart]);
+  }
+  return c;
 }
-
+command_stream_t initiateCmd(){
+  // finish this later
+  command_stream_t cmd = new struct command_stream();
+  cmd->
+}
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
-                     void *get_next_byte_argument)
+		     void *get_next_byte_argument)
 {
-    /* FIXME: Replace this with your implementation.  You may need to
+  /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-    error (1, 0, "command reading not yet implemented");
-    return 0;
+  char cmd[10000];
+  int leng = 0;
+  command_stream_t head;
+  while (1){
+      /* seperate the cmd by newline for now */
+      leng = 0;
+      while (c = get_next_byte(get_next_byte_argument) != NEW_LINE){
+          if (c == EOF) 
+              break;
+          cmd[leng] = c;
+      }
+      stream->command = parseCmd(cmd);
+      stream->next = ;
+  }
+  error (1, 0, "command reading not yet implemented");
+  return 0;
 }
 
 command_t
 read_command_stream (command_stream_t s)
 {
-    /* FIXME: Replace this with your implementation too.  */
-    //error (1, 0, "command reading not yet implemented");
-    if (s != NULL) {
-        command_t command = s.command;
-        s = s.next;
-        return command;
-    }
-    else {
-        return NULL;
-    }
-    //return 0;
+  /* FIXME: Replace this with your implementation too.  */
+  error (1, 0, "command reading not yet implemented");
+  return 0;
 }
+
+/*
+*  To-do:
+      1. separate cmd;
+      2. determine cmd;
+      3. deal with each cmd type;
+      
+*/
