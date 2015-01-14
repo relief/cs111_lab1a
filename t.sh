3d63844dc69eed8d532c2e921be4dc669cc80f93
guayang <guanya.yang@gmail.com>
2015-01-13 21:12:18 -0800

    added new testcase

--- t.sh
+++ t.sh
@@ -1,46 +1,9 @@
-true
-
-g++ -c foo.c
-
-: : :
-
-if cat < /etc/passwd | tr a-z A-Z | sort -u; then :; else echo sort failed!; fi
-
-a b<c > d
-
-if cat < /etc/passwd | tr a-z A-Z | sort -u > out
-then :
-else echo sort failed!
-fi
-
-<<<<<<< HEAD
 a | b
 
 while b do p done
 
 (a) | b
-=======
-if
-  if a;a;a; then b; else :; fi
-then
->>>>>>> 6b4c76d8b16c6f26ce092a5c062f8e28f5b0f7f9
 
- if c
-  then if d | e; then f; fi
- fi
-fi
+until asdf do adfsj done; (yahoo) | d
 
-g<h
-
-while
-  while
-    until :; do echo yoo hoo!; done
-    false
-  do (a|b)
-  done >f
-do
-  :>g
-done
-
-# Another weird example: nobody would ever want to run this.
-a<b>c|d<e>f|g<h>i
+a<b
